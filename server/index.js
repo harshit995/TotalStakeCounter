406062dfd2bbad534aa859bcc0b1bacfa3792821
Harshit <gharshit89@gmail.com>
2023-08-02 17:24:41 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(router);
